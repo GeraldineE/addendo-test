3bd8d910ff432f7b8ecd35b790c894672fd7fc2e
Geraldine Echavarria <geraldine.e.cortes@gmail.com>
2020-06-30 16:28:52 -0500

    Add serializers

---
+++ api/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Organization, Consultant
+
+
+class OrganizationSerializer(ModelSerializer):
+    class Meta:
+        model = Organization
+        fields = "__all__"
+
+
+class ConsultantSerializer(ModelSerializer):
+    class Meta:
+        model = Consultant
+        fields = "__all__"
+        depth = 1
+
