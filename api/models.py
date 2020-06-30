5b68ab3ed57425f67421841f92ea739848c648d2
Geraldine Echavarria <geraldine.e.cortes@gmail.com>
2020-06-30 16:28:39 -0500

    Add models

--- api/models.py
+++ api/models.py
@@ -4,10 +4,23 @@ from django.db import models
 class Organization(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    consultants = models.M
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"<Organization:{self.name}>"
 
 
 class Consultant(models.Model):
     name = models.CharField(max_length=100)
     brithday = models.DateField()
-    organization = models.ForeignKey(Organization, related_name="consultants")
+    organization = models.ForeignKey(
+        Organization, on_delete=models.CASCADE, related_name="consultants"
+    )
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"<Consultant:{self.name}>"
